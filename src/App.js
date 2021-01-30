1c65c9b9afb0fe0addd5b5bb60941793577e7370
Anna Chirapongse <achirapongse@gmail.com>
2021-01-29 21:39:00 -0500

    Updates for the portfolio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+import Contact from "./pages/contact";
+import Portfolio from "./pages/portfolio";
 import Nav from "./components/Nav";
 
 function App() {
@@ -11,10 +10,10 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path={["/", "/books"]}>
+          <Route exact path={["/", "/portoflio"]}>
             <Books />
           </Route>
-          <Route exact path="/books/:id">
+          <Route exact path={["/", "/contact"]}>
             <Detail />
           </Route>
           <Route>
